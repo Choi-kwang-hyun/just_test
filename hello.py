58da8b8a5f2b3f9ac6b20ec3c7adfb70e674f293
Choi-kwang-hyun <59353752+Choi-kwang-hyun@users.noreply.github.com>
2020-10-19 13:43:33 +0900

    Update hello.py
    ddd

--- hello.py
+++ hello.py
@@ -1,2 +1,21 @@
 # hello.py
-print("Hello world")
+
+import json
+
+from github3 import login
+
+@csrf_exempt
+def post_list(request):
+    jsondata = request.body
+    print(jsondata)
+
+    d = json.loads(jsondata)
+    state = d['action']
+    issues = d['issue']
+
+    title = issues['title']
+    body = issues['body']
+    label = issues['labels']
+    number = issues['number']
+    
+    print(body)
